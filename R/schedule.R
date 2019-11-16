726e942ec512ca9157c586bcbb1ceec8343111e5
Colin Fraser <colin.r.fraser@gmail.com>
2019-11-15 16:34:45 -0800

    styler linting

--- R/schedule.R
+++ R/schedule.R
@@ -3,34 +3,34 @@ library(rvest)
 library(lubridate)
 
 URL <- "https://www.basketball-reference.com/leagues/NBA_2020_games-{month}.html"
-source('R/paths.R')
+source("R/paths.R")
 
 get_schedule <- function(month) {
   url <- str_glue(URL, month = month)
-  
-  raw_table <- read_html(url) %>% 
-    html_node('table')
-  
-  game_info <- raw_table %>% 
-    html_nodes('.left:nth-child(3)') %>% 
-    html_attr('csk')
-  
+
+  raw_table <- read_html(url) %>%
+    html_node("table")
+
+  game_info <- raw_table %>%
+    html_nodes(".left:nth-child(3)") %>%
+    html_attr("csk")
+
   visitor <- str_sub(game_info, 1, 3)
   home <- str_sub(game_info, -3)
-  dates <- str_sub(game_info, 5, 12) %>% 
-    ymd
+  dates <- str_sub(game_info, 5, 12) %>%
+    ymd()
   schedule <- tibble(
     date = dates,
-    visitor = visitor, 
+    visitor = visitor,
     home = home
   )
-  
+
   schedule
 }
 
-schedule <- month.name %>% 
-  str_to_lower() %>% 
-  .[c(10:12, 1:4)] %>% 
+schedule <- month.name %>%
+  str_to_lower() %>%
+  .[c(10:12, 1:4)] %>%
   map_df(get_schedule)
 
 if (!dir.exists(dirname(SCHEDULE_PATH))) {
@@ -38,4 +38,3 @@ if (!dir.exists(dirname(SCHEDULE_PATH))) {
 }
 
 write_csv(schedule, SCHEDULE_PATH)
-
